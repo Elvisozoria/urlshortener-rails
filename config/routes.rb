6fce2e83de3101aee9e29685b897599bd4b7fac7
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-04-07 19:22:18 -0400

    renames the add route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post 'api/v1/urls/add/:original_url', to: 'urls#new'
-  get 'api/v1/urls/add/:original_url', to: 'urls#new'
+  get 'api/v1/urls/add/', to: 'urls#new'
   get 'api/v1/urls/top', to: 'urls#top'
   get '/s/:shorten_url', to: 'urls#redirect', as: :redirect
 
