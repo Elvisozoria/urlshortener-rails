3ed5f6ddfbfed79094b1d02734c88c0e11b7975e
Elvis Ozoria <elvis.ozoria@gmail.com>
2021-04-07 19:07:17 -0400

    adds redirect and new  methods

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,42 @@
 class UrlsController < ApplicationController 
+
+    def redirect
+        if params['shorten_url']
+            if Url.find_by_shorten_url(params['shorten_url'])
+                @url = Url.find_by_shorten_url(params['shorten_url'])
+                redirect_to @url.original_url
+                # render json: @url
+            end
+            
+        end
+        
+    end
+    
+    def new
+        if params['original_url']
+            @url= Url.create(original_url:params['original_url'])
+            @url.encode
+            if @url.save
+                @url.get_title
+                render json: @url.shorten_url, status: :created
+            else
+            render json: @url.errors, status: :unprocessable_entity
+            end
+        end
+
+      
+    end
+
+    def top
+        render json: @url, status: :created
+
+    end
+    
+
+    private 
+    def valid_url?(url)
+
+    end
+
+
 end
